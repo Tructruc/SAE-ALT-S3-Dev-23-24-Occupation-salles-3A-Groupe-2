c0f8b95f92ae2f8c79fcaf1708b180bdd0ff9723
Fournet Enzo <enzofournet31@gmail.com>
2024-01-10 19:23:46 +0100

    suite mise en place API
    #12

--- backend/API/project/urls.py
+++ backend/API/project/urls.py
@@ -21,6 +21,8 @@ from rest_framework import routers
 from django.conf.urls.static import static
 from django.conf import settings
 
+from app.usecases.mqttlistener import MqttClientThread
+
 router = routers.DefaultRouter()
 DeepViewSet.init_router(router, [
     Data,
@@ -30,3 +32,18 @@ DeepViewSet.init_router(router, [
 urlpatterns = [
     re_path(r'', include(router.urls)),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+import sys
+
+if 'runserver' in sys.argv : # Only run the MQTT client when running the server
+    print("Démarrage des threads MQTT")
+
+    mqtt_thread_1 = MqttClientThread("application/1/device/+/event/status")
+    mqtt_thread_1.daemon = True
+    mqtt_thread_1.start()
+    print("Thread 1 started")
+
+    mqtt_thread_2 = MqttClientThread("AM107/by-room/#")
+    mqtt_thread_2.daemon = True
+    mqtt_thread_2.start()
+    print("Thread 2 started")
