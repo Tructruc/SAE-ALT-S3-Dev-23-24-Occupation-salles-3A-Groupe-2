61d8315f111b7748482e209f94aeb071f7c90155
Fournet Enzo <enzofournet31@gmail.com>
2024-01-10 08:41:14 +0100

    rebDébut mise en palce de l'API Django

--- backend/API/app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from app.usecases.mqttlistener import MqttClientThread
-
-
-class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
