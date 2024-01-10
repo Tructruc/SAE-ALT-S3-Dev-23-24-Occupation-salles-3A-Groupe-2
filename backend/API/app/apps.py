5eb169448b73e63671c899a2d20f1d1356987551
Fournet Enzo <enzofournet31@gmail.com>
2024-01-10 11:39:35 +0100

    Mise en place API
    nodered supprimé.
    Ajout de adminer pour le dev.
    Mise ne place des listener MQTT sur l'API et des options nécessaire.

--- backend/API/app/apps.py
+++ backend/API/app/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
+from app.usecases.mqttlistener import MqttClientThread
 
 
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+
+    def ready(self):
+        import os
+        if os.environ.get('RUN_MAIN') != 'true':
+            print("go")
+            mqtt_thread_1 = MqttClientThread("application/1/device/+/event/status")
+            print("la")
+            mqtt_thread_1.start()
+            print("laal")
+
+            print("gogo")
+            mqtt_thread_2 = MqttClientThread("AM107/by-room/#")
+            mqtt_thread_2.start()
