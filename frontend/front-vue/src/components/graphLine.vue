b7790066866cd4b5803742d734f2944adb7c9ded
Tructruc <emilien.fieu@etu.univ-tlse2.fr>
2024-01-11 09:45:47 +0100

    Ajout du composant de graphique et suppression du projet par default
    #13

---
+++ frontend/front-vue/src/components/graphLine.vue
@@ -0,0 +1,38 @@
+<template>
+  <Line :data="data" :options="options" style="background-color: white;height: 400px"/>
+</template>
+
+<script lang="ts">
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+import * as chartConfig from './chartConfig.js'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+export default {
+  name: 'App',
+  components: {
+    Line
+  },
+  data() {
+    return chartConfig
+  }
+}
+</script>
