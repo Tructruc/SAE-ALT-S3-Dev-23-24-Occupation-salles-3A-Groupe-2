d438b670d4da47186e31e7377efe7650b42bc546
Fournet Enzo <enzofournet31@gmail.com>
2024-01-17 14:33:56 +0100

    Reconnexion & logging mqttlistener
    Ajout de la reconnexion toute les 10 secondes si le Brocker mqtt n'est pas joignable.
    #30

--- backend/API/app/usecases/mqttlistener.py
+++ backend/API/app/usecases/mqttlistener.py
@@ -1,8 +1,8 @@
 import paho.mqtt.client as mqtt
 import multiprocessing
 import json
-from pprint import pprint
 import logging
+import time
 
 logger = logging.getLogger('API')
 
@@ -11,44 +11,40 @@ class MqttClientProcess(multiprocessing.Process):
         super().__init__()
         self.client = mqtt.Client()
         self.topic = topic
-
-        # Utilisez des fonctions lambda pour passer les arguments supplémentaires
         self.client.on_connect = lambda client, userdata, flags, rc: self.on_connect(client, userdata, flags, rc, self.topic)
         self.client.on_message = lambda client, userdata, msg: self.on_message(client, userdata, msg, self.topic)
+        self.is_running = True
 
     def on_connect(self, client, userdata, flags, rc, topic):        
-        logger.info("Connected with result code " + str(rc) + f" to topic {topic}")
+        logger.info(f"Connexion au brocker MQTT réussie sur le topic: {topic}")
         client.subscribe(topic)
 
     def on_message(self, client, userdata, msg, topic):
         from app.usecases import create_sensor
         try:
-            # Décodez le payload et convertissez-le en dictionnaire
             message_dict = json.loads(msg.payload.decode('utf-8'))
-            # Affichez le message de manière formatée
-            logger.info(f"Received message on {topic}: ")
-
+            logger.debug(f"Message reçus sur {topic}")
+            logger.debug(message_dict)
             create_sensor(message_dict, topic)
-
         except json.JSONDecodeError:
-            print(f"Error decoding JSON: {msg.payload}")
+            logger.error(f"Erreur inconnu lors de la désérialisation du message")
 
     def run(self):
-        try:
-            # Tenter de se connecter au broker MQTT
-            self.client.connect("chirpstack.iut-blagnac.fr", 1883, 60)
-        except TimeoutError as e:
-            # Gérer l'exception TimeoutError ici
-            print("La connexion a dépassé le délai imparti. Vérifiez l'adresse du serveur et votre connexion internet.")
-            # Ajouter une logique de reconnexion ou terminer le programme
-        except Exception as e:
-            # Gérer d'autres exceptions potentielles
-            print(f"Une exception a été levée: {e}")
-        self.client.loop_start()
+        logger.debug("Tentative de connexion au brocker MQTT")
+        while self.is_running:
+            try:
+                self.client.connect("chirpstack.iut-blagnac.fr", 1883, 60)
+                self.client.loop_start()
+                while self.is_running:
+                    time.sleep(1)
+            except TimeoutError:
+                logger.info("La connexion au brocker MQTT a dépassé le délai imparti. Vérifiez l'adresse du serveur et votre connexion internet.")
+                time.sleep(10) # Wait 10 seconds before retrying
+            except KeyboardInterrupt:
+                logger.info(f"Arrêt du processus MQTT du topic {self.topic}")
+                self.is_running = False
+            except Exception as e:
+                logger.error(f"Erreur inconnue lors de la connexion au brocker MQTT: {e}")
+                time.sleep(10)
 
-        try:
-            while True:
-                pass
-        except KeyboardInterrupt:
-            print("Arrêt du processus.")
-            self.client.disconnect()
+        self.client.disconnect()
