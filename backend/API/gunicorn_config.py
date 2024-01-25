368d24e96a405c7bbacd93493d1466cb86151b2c
Fournet Enzo <enzofournet31@gmail.com>
2024-01-25 13:39:39 +0100

    FIX gestion lancement MQTT listener en prod

--- backend/API/gunicorn_config.py
+++ backend/API/gunicorn_config.py
@@ -1,8 +1,21 @@
+import multiprocessing
+import os
+
 from project.loggerconfig import setup_logger, setup_gunicorn_loggers
 from project.settings.prod_settings import DEFAULT_IP, DEFAULT_PORT
+from app.processmqttlistenerstarter import start_process_mqtt_listener
+from django.core.wsgi import get_wsgi_application
+
 
 bind = f"{DEFAULT_IP}:{DEFAULT_PORT}"
+custom_logger = setup_logger()
+workers = multiprocessing.cpu_count() * 2 + 1
 
 def on_starting(server):
-    custom_logger = setup_logger()
-    setup_gunicorn_loggers(custom_logger)
+    setup_gunicorn_loggers(custom_logger)    
+
+def when_ready(server):
+    custom_logger.info("Starting MQTT listener process")
+    os.environ.get("DJANGO_SETTINGS_MODULE")
+    get_wsgi_application()
+    start_process_mqtt_listener()
