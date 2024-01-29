54960847a10839210609f188da1cb8bd163a4c7b
Fournet Enzo <enzofournet31@gmail.com>
2024-01-29 22:51:12 +0100

    Update eventsdescription.py

--- backend/API/app/views/eventsdescription.py
+++ backend/API/app/views/eventsdescription.py
@@ -1,8 +1,6 @@
 from django.http import JsonResponse
-from app.usecases.redis import redis_sender
 
 def events_description(request):    
-    redis_sender('Data', 'TEST')
     return JsonResponse({
         "endpoints": {
             "Sensor": "/Events/Sensor/",
